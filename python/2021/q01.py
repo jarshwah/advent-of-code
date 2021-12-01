5228ca1178dd57ca93baa8976406755c285bf4eb
Josh Smeaton <josh.smeaton@gmail.com>
2021-12-01 16:20:36 +1100

    2021 - Day 1

---
+++ python/2021/q01.py
@@ -0,0 +1,44 @@
+import aocd
+
+import utils
+
+
+def part_one(numbers):
+    """
+    Find the count of increasing depth given a list of depths
+    """
+    return sum(pair[1] > pair[0] for pair in zip(numbers, numbers[1:]))
+
+
+def part_two(numbers):
+    """
+    Find the count of increasing depth for a 3 dimensional sliding window of depth sums
+    """
+    windows = list(zip(numbers, numbers[1:], numbers[2:]))
+    return sum(sum(pair[1]) > sum(pair[0]) for pair in zip(windows, windows[1:]))
+
+
+def test():
+    numbers = [
+        199,
+        200,
+        208,
+        210,
+        200,
+        207,
+        240,
+        269,
+        260,
+        263,
+    ]
+    answer_1 = part_one(numbers)
+    answer_2 = part_two(numbers)
+    assert answer_1 == 7, answer_1
+    assert answer_2 == 5, answer_2
+
+
+if __name__ == "__main__":
+    test()
+    numbers = utils.int_numbers(aocd.get_data(day=1, year=2021))
+    print(part_one(numbers))
+    print(part_two(numbers))
