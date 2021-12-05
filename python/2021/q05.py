2c0599798cdfe53b53397900007f11bc3e636c7d
Josh Smeaton <josh.smeaton@gmail.com>
2021-12-05 22:57:54 +1100

    2021 - Day 5 - Python

---
+++ python/2021/q05.py
@@ -0,0 +1,49 @@
+import itertools
+import typing as t
+from collections import Counter
+
+import aocd
+
+from utils import Point, line_algorithm
+
+
+def parse(data: str) -> t.Iterable[tuple[Point, Point]]:
+    for line in data.splitlines():
+        l, r = line.split(" -> ")
+        yield [tuple(map(int, l.split(","))), tuple(map(int, r.split(",")))]
+
+
+def part_one(data: str) -> int:
+    # Horizontal / Vertical lines only
+    points = [p for p in parse(data) if p[0][0] == p[1][0] or p[0][1] == p[1][1]]
+    danger_zone = Counter(itertools.chain(*(line_algorithm(x, y) for x, y in points)))
+    return len([k for k, v in danger_zone.items() if v >= 2])
+
+
+def part_two(data: str) -> int:
+    danger_zone = Counter(itertools.chain(*(line_algorithm(x, y) for x, y in parse(data))))
+    return len([k for k, v in danger_zone.items() if v >= 2])
+
+
+def test():
+    test_input = """0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2"""
+    answer_1 = part_one(test_input)
+    answer_2 = part_two(test_input)
+    assert answer_1 == 5, answer_1
+    assert answer_2 == 12, answer_2
+
+
+if __name__ == "__main__":
+    test()
+    data = aocd.get_data(day=5, year=2021)
+    print("Part 1: ", part_one(data))
+    print("Part 2: ", part_two(data))
--- python/utils.py
+++ python/utils.py
@@ -1,6 +1,7 @@
 import typing as t
 
 G = t.TypeVar("G")
+Point = tuple[float, float]
 
 
 def int_numbers(input_data: str) -> t.List[int]:
@@ -22,3 +23,36 @@ def only(i: t.Iterable[G]) -> G:
 
 def sort_by_length(iterables: t.Iterable[t.Sequence[G]]) -> t.Iterable[t.Sequence[G]]:
     return sorted(iterables, key=len)
+
+
+def line_algorithm(start: Point, end: Point) -> t.Iterable[Point]:
+    """
+    Finds all points between `start` and `end` points.
+    """
+    # attribution: https://github.com/encukou/bresenham
+    x0, y0 = start
+    x1, y1 = end
+
+    dx = x1 - x0
+    dy = y1 - y0
+
+    xsign = 1 if dx > 0 else -1
+    ysign = 1 if dy > 0 else -1
+    dx = abs(dx)
+    dy = abs(dy)
+
+    if dx > dy:
+        xx, xy, yx, yy = xsign, 0, 0, ysign
+    else:
+        dx, dy = dy, dx
+        xx, xy, yx, yy = 0, ysign, xsign, 0
+
+    D = 2 * dy - dx
+    y = 0
+
+    for x in range(dx + 1):
+        yield x0 + x * xx + y * yx, y0 + x * xy + y * yy
+        if D >= 0:
+            y += 1
+            D -= 2 * dx
+        D += 2 * dy
