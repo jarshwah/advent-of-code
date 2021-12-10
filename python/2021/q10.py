391cf7ab7bce76316772633f969375683915a421
Josh Smeaton <josh.smeaton@gmail.com>
2021-12-10 21:43:25 +1100

    2021 - Day 10

---
+++ python/2021/q10.py
@@ -0,0 +1,56 @@
+from functools import reduce
+from statistics import median
+
+import aocd
+
+OPEN = "([{<"
+CLOSE = ")]}>"
+POINTS = [3, 57, 1197, 25137]
+
+
+def flip(ch: str) -> str:
+    if ch in CLOSE:
+        return OPEN[CLOSE.index(ch)]
+    return CLOSE[OPEN.index(ch)]
+
+
+def both_parts(lines: str) -> tuple[int, int]:
+    a1 = 0
+    a2 = []
+    for line in lines:
+        stack = []
+        for ch in line:
+            if ch in OPEN:
+                stack.append(ch)
+                continue
+            if flip(ch) != stack.pop():
+                a1 += POINTS[CLOSE.index(ch)]
+                break
+        else:
+            a2.append(reduce(lambda cum, item: cum * 5 + OPEN.index(item) + 1, reversed(stack), 0))
+    return a1, int(median(sorted(a2)))
+
+
+def test():
+    test_input = """[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]"""
+
+    answer_1, answer_2 = both_parts(test_input.splitlines())
+    assert answer_1 == 26397, answer_1
+    assert answer_2 == 288957, answer_2
+
+
+if __name__ == "__main__":
+    test()
+    data = aocd.get_data(day=10, year=2021)
+    p1, p2 = both_parts(data.splitlines())
+    print("Part 1: ", p1)
+    print("Part 2: ", p2)
