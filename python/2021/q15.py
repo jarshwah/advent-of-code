72298705d3f9175f1c9909d0c732f9b0296e0322
Josh Smeaton <josh.smeaton@gmail.com>
2021-12-15 21:28:43 +1100

    Day 15 - Python - Part 2 fixed

--- python/2021/q15.py
+++ python/2021/q15.py
@@ -10,41 +10,35 @@ from utils import Grid, Point
 def build_graph(data: str) -> tuple[nx.DiGraph, Point, Point, Point]:
     grid = Grid(rows=((int(n) for n in row) for row in data.splitlines()))
     first_stop = max(grid)
-    for y in range(first_stop[1]):  # 9
-        for x in range(first_stop[0]):  # 9
-            for n in range(1, 5):  # 4
-                # increase size of grid down
-                newy = first_stop[1] * n + y + 1  # 9 * 4 (36) + 9 (45) + 1 = 46
-                # increase size of grid right
-                newx = first_stop[0] * n + x + 1  # 9 * 4 (36) + 9 (45) + 1 = 46
-                nweight = grid[x, y] + n
-                if nweight > 9:
-                    nweight -= 9
-                grid[x, newy] = nweight
-                grid[newx, y] = nweight
-                grid[newx, newy] = nweight
+    lx = first_stop[0] + 1
+    ly = first_stop[1] + 1
+    for y in range(ly):
+        for x in range(lx):
+            for yd in range(0, 5):
+                for xr in range(0, 5):
+                    if yd == xr == 0:
+                        continue
+                    newy = ly * yd + y
+                    newx = lx * xr + x
+                    weight = grid[x, y] + xr + yd
+                    if weight > 9:
+                        weight -= 9
+                    grid[newx, newy] = weight
 
     graph = nx.DiGraph()
-    edge_count = 0
     for point in grid:
         neighbours = grid.get_neighbours(point)
         for nb in neighbours:
-            edge_count += 1
             graph.add_edge(point, nb, weight=grid[nb])
-    print(edge_count)
     return graph, (0, 0), first_stop, max(grid)
 
 
-def part_one(data: str) -> int:
-    graph, start, end, _ = build_graph(data)
-    shortest = single_source_dijkstra(graph, start, end)
-    return shortest[0]
+def part_one(graph: nx.DiGraph, start: Point, target: Point) -> int:
+    return single_source_dijkstra(graph, start, target)[0]
 
 
-def part_two(data: str) -> int:
-    graph, start, _, end = build_graph(data)
-    shortest = single_source_dijkstra(graph, start, end)
-    return shortest[0]
+def part_two(graph: nx.DiGraph, start: Point, target: Point) -> int:
+    return single_source_dijkstra(graph, start, target)[0]
 
 
 def test():
@@ -58,8 +52,9 @@ def test():
 3125421639
 1293138521
 2311944581"""
-    answer_1 = part_one(test_input)
-    answer_2 = part_two(test_input)
+    graph, start, p1, p2 = build_graph(test_input)
+    answer_1 = part_one(graph, start, p1)
+    answer_2 = part_two(graph, start, p2)
     assert answer_1 == 40, answer_1
     assert answer_2 == 315, answer_2
 
@@ -67,5 +62,6 @@ def test():
 if __name__ == "__main__":
     test()
     data = aocd.get_data(day=15, year=2021)
-    print("Part 1: ", part_one(data))
-    print("Part 2: ", part_two(data))
+    graph, start, p1, p2 = build_graph(data)
+    print("Part 1: ", part_one(graph, start, p1))
+    print("Part 2: ", part_two(graph, start, p2))
