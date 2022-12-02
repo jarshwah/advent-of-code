b7fc2f25ee05d645e95e757b2db464bc9c6c5d84
Josh Smeaton <josh.smeaton@gmail.com>
2022-12-02 17:01:58 +1100

    Day 2 - 2022 - Complete

--- python/2022/q02.py
+++ python/2022/q02.py
@@ -1,25 +1,71 @@
-import typing as t
-
 import aocd
 import utils
 
+table = {
+    "A": 1,  # rock
+    "B": 2,  # paper
+    "C": 3,  # scissors
+    # -----------------
+    "X": 1,  # rock/lose
+    "Y": 2,  # paper/draw
+    "Z": 3,  # scissors/win
+}
+
+
+def play(oppo: int, us: int) -> int:
+    """
+    Return the score of playing the game.
+
+    Our score + 6 for a win, 3 for a draw, 0 for a loss
+    """
+    if oppo == us:  # draw
+        return 3 + us
+    if (us - oppo) % 3 == 1:  # win
+        return 6 + us
+    return us  # lose
+
+
+def play_two(them: int, us: int):
+    """
+    Return the score of playing the game.
+
+    Us: 2 = draw, 1 = lose, 3 = win
+    """
+    if us == 2:  # draw
+        return 3 + them
+    if us == 1:  # lose
+        return (them - 1) or 3
+    return (1 if them == 3 else them + 1) + 6  # win
+
 
 def part_one(raw: str) -> int:
-    data = utils.Input(raw)
-    return 1
+    data = utils.Input(raw).group("\n").strings
+    score = []
+    for opponent, us in data:
+        oscore = table[opponent]
+        usscore = table[us]
+        score.append(play(oscore, usscore))
+    return sum(score)
 
 
 def part_two(raw: str) -> int:
-    data = utils.Input(raw)
-    return 1
+    data = utils.Input(raw).group("\n").strings
+    score = []
+    for opponent, us in data:
+        oscore = table[opponent]
+        usscore = table[us]
+        score.append(play_two(oscore, usscore))
+    return sum(score)
 
 
 def test():
-    test_input = """"""
+    test_input = """A Y
+B X
+C Z"""
     answer_1 = part_one(test_input)
     answer_2 = part_two(test_input)
-    assert answer_1 == 1, answer_1
-    assert answer_2 == 1, answer_2
+    assert answer_1 == 15, answer_1
+    assert answer_2 == 12, answer_2
 
 
 if __name__ == "__main__":
