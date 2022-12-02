7f356bffd941afea3f978aa40fe721eb367fe150
Josh Smeaton <josh.smeaton@gmail.com>
2022-12-02 17:28:28 +1100

    Inline functions and simplify

--- python/2022/q02.py
+++ python/2022/q02.py
@@ -12,50 +12,34 @@ table = {
 }
 
 
-def play(oppo: int, us: int) -> int:
-    """
-    Return the score of playing the game.
-
-    Our score + 6 for a win, 3 for a draw, 0 for a loss
-    """
-    if oppo == us:  # draw
-        return 3 + us
-    if (us - oppo) % 3 == 1:  # win
-        return 6 + us
-    return us  # lose
-
-
-def play_two(them: int, us: int):
-    """
-    Return the score of playing the game.
-
-    Us: 2 = draw, 1 = lose, 3 = win
-    """
-    if us == 2:  # draw
-        return 3 + them
-    if us == 1:  # lose
-        return (them - 1) or 3
-    return (1 if them == 3 else them + 1) + 6  # win
-
-
 def part_one(raw: str) -> int:
     data = utils.Input(raw).group("\n").strings
-    score = []
+    score = 0
     for opponent, us in data:
-        oscore = table[opponent]
-        usscore = table[us]
-        score.append(play(oscore, usscore))
-    return sum(score)
+        them, ours = table[opponent], table[us]
+        if them == ours:  # draw
+            score += 3 + ours
+            continue
+        elif (ours - them) % 3 == 1:  # win
+            score += 6 + ours
+            continue
+        score += ours  # lose
+    return score
 
 
 def part_two(raw: str) -> int:
     data = utils.Input(raw).group("\n").strings
-    score = []
+    score = 0
     for opponent, us in data:
-        oscore = table[opponent]
-        usscore = table[us]
-        score.append(play_two(oscore, usscore))
-    return sum(score)
+        them, ours = table[opponent], table[us]
+        if ours == 2:  # draw
+            score += 3 + them
+            continue
+        elif ours == 1:  # lose
+            score += (them - 1) or 3
+            continue
+        score += (1 if them == 3 else them + 1) + 6  # win
+    return score
 
 
 def test():
