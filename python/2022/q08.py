e57bab80008e3dd8070c517424037b0f40930ff0
Josh Smeaton <josh.smeaton@gmail.com>
2022-12-09 20:39:43 +1100

    2022 Day 8 Complete
    Given a grid of tree heights determine the visibility of each tree
    from a point of reference.
    
    Part 1: count the number of visible trees from outside the grid. We
    scan from the left, then the top, then from right and finally from the
    bottom, counting only trees that are smaller than the max already found.
    
    Scanning in this manner is O(n) as we visit each node just once.
    
    Part 2: count the number of trees visible from each tree, stopping once
    we hit a tree of same or greater height. This is wildly inefficient as
    we rescan nodes over and over.
    
    We could store the visibility in each direction for each node to avoid
    some recomputation, but the solution is already found in a reasonable
    amount of time so I'm moving on.

--- python/2022/q08.py
+++ python/2022/q08.py
@@ -1,26 +1,85 @@
-import typing as t
-
 import aocd
 import utils
 
 
-# this is just a test
 def part_one(raw: str) -> int:
-    data = utils.Input(raw)
-    return 1
+    grid = utils.Grid.from_number_string(raw)
+    can_see = set()
+    width = max(grid)[0] + 1
+    # look from the left
+    for row_num in range(width):
+        prev = -1
+        for col_num in range(width):
+            location = row_num, col_num
+            if grid[location] > prev:
+                can_see.add(location)
+                prev = max(grid[location], prev)
+                continue
+
+    # look from top
+    for row_num in range(width):
+        prev = -1
+        for col_num in range(width):
+            location = col_num, row_num
+            if grid[location] > prev:
+                can_see.add(location)
+                prev = max(grid[location], prev)
+                continue
+
+    # look from right
+    for row_num in range(width - 1, -1, -1):
+        prev = -1
+        for col_num in range(width - 1, -1, -1):
+            location = row_num, col_num
+            if grid[location] > prev:
+                can_see.add(location)
+                prev = max(grid[location], prev)
+                continue
+
+    # look from bottom
+    for row_num in range(width - 1, -1, -1):
+        prev = -1
+        for col_num in range(width - 1, -1, -1):
+            location = col_num, row_num
+            if grid[location] > prev:
+                can_see.add(location)
+                prev = max(grid[location], prev)
+                continue
+
+    return len(can_see)
 
 
 def part_two(raw: str) -> int:
-    data = utils.Input(raw)
-    return 1
+    grid = utils.Grid.from_number_string(raw)
+    best_view = 0
+    for point in grid:
+        view = 1
+        height = grid[point]
+        for direction in utils.DIRECTIONS_4:
+            current = point
+            count = 0
+            while True:
+                current = utils.sum_points(current, direction)
+                if current not in grid:
+                    break
+                count += 1
+                if grid[current] >= height:
+                    break
+            view *= count
+        best_view = max(best_view, view)
+    return best_view
 
 
 def test():
-    test_input = """"""
+    test_input = """30373
+25512
+65332
+33549
+35390"""
     answer_1 = part_one(test_input)
     answer_2 = part_two(test_input)
-    assert answer_1 == 1, answer_1
-    assert answer_2 == 1, answer_2
+    assert answer_1 == 21, answer_1
+    assert answer_2 == 8, answer_2
 
 
 if __name__ == "__main__":
