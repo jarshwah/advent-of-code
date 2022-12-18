98e5e5e04c64db634bcc926fc32543bbb6287fe1
Josh Smeaton <josh.smeaton@gmail.com>
2022-12-19 09:16:07 +1100

    Tidy up of day 18

--- python/2022/q18.py
+++ python/2022/q18.py
@@ -16,20 +16,20 @@ def neighbours(cube: tuple[int, int, int]) -> set[tuple[int, int, int]]:
 
 def solve(raw: str) -> tuple[int, int]:
     data = utils.Input(raw).group("\n", ",").integers
-    cubes = {(x, y, z) for x, y, z in data}
+    cubes = {tuple(coords) for coords in data}
     surfaces = 6 * len(cubes)
-    empty = 0
+
     for cube in cubes:
         surfaces -= len([n for n in neighbours(cube) if n in cubes])
 
     # create empty space around the grid so we can traverse all
     # the way around and try to enter all empty spaces
-    minx = min(x for x, y, z in cubes) - 1
-    maxx = max(x for x, y, z in cubes) + 2
-    miny = min(y for x, y, z in cubes) - 1
-    maxy = max(y for x, y, z in cubes) + 2
-    minz = min(z for x, y, z in cubes) - 1
-    maxz = max(z for x, y, z in cubes) + 2
+    minx = min(c[0] for c in cubes) - 1
+    maxx = max(c[0] for c in cubes) + 2
+    miny = min(c[1] for c in cubes) - 1
+    maxy = max(c[1] for c in cubes) + 2
+    minz = min(c[2] for c in cubes) - 1
+    maxz = max(c[2] for c in cubes) + 2
 
     grid = {
         (x, y, z) for x in range(minx, maxx) for y in range(miny, maxy) for z in range(minz, maxz)
@@ -40,10 +40,9 @@ def solve(raw: str) -> tuple[int, int]:
         check = queue.pop()
         if check in missing:
             missing.remove(check)
-            queue.extend(nb for nb in neighbours(check) if nb in grid)
-    for cube in missing:
-        empty -= len([n for n in neighbours(cube) if n in cubes])
-    return surfaces, surfaces + empty
+            queue.extend(neighbours(check))
+    empty = sum(len([n for n in neighbours(cube) if n in cubes]) for cube in missing)
+    return surfaces, surfaces - empty
 
 
 def test():
