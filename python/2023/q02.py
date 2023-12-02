b413a909e1358acc86f2bbbe8dbf6939fc9d9fbb
Josh Smeaton <josh.smeaton@gmail.com>
2023-12-02 15:55:57 +1100

    Begin configuring pytest

---
+++ python/2023/q02.py
@@ -0,0 +1,27 @@
+import aocd
+import utils
+
+
+def part_one(raw: str) -> int:
+    data = utils.Input(raw)
+    return 1
+
+
+def part_two(raw: str) -> int:
+    data = utils.Input(raw)
+    return 1
+
+
+def test():
+    test_input = """"""
+    answer_1 = part_one(test_input)
+    answer_2 = part_two(test_input)
+    assert answer_1 == 1, answer_1
+    assert answer_2 == 1, answer_2
+
+
+if __name__ == "__main__":
+    test()
+    data = aocd.get_data(day=2, year=2023)
+    print("Part 1: ", part_one(data))
+    print("Part 2: ", part_two(data))
