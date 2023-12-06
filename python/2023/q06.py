5f785d794d221068127892c9f93148187acc77ed
Josh Smeaton <josh.smeaton@gmail.com>
2023-12-06 16:55:56 +1100

    2023 - Day 6 - Part 2 alternative

--- python/2023/q06.py
+++ python/2023/q06.py
@@ -25,11 +25,22 @@ def part_two(raw: str) -> int:
     return sum(1 for attempt in range(1, time) if attempt * (time - attempt) > distance)
 
 
+def part_two_alt(raw: str) -> int:
+    # Halve the search space - check from the midpoint and return when we exceed
+    # the distance, doubling the result.
+    time, distance = [
+        int(s.split(":")[1].replace(" ", "")) for s in utils.Input(raw).lines().strings
+    ]
+    for idx, attempt in enumerate(range(time // 2, time)):
+        if attempt * (time - attempt) <= distance:
+            return idx * 2 - 1
+
+
 def test():
     test_input = """Time:      7  15   30
 Distance:  9  40  200"""
     answer_1 = part_one(test_input)
-    answer_2 = part_two(test_input)
+    answer_2 = part_two_alt(test_input)
     assert answer_1 == 288, answer_1
     assert answer_2 == 71503, answer_2
 
@@ -38,4 +49,4 @@ if __name__ == "__main__":
     test()
     data = aocd.get_data(day=6, year=2023)
     print("Part 1: ", part_one(data))
-    print("Part 2: ", part_two(data))
+    print("Part 2: ", part_two_alt(data))
