7e7d27828c9891dc4978b1ef78bab73df8b2d3f7
Josh Smeaton <josh.smeaton@gmail.com>
2023-12-11 17:15:54 +1100

    2023 - Day 11 - Cleanup

--- python/2023/q11.py
+++ python/2023/q11.py
@@ -30,7 +30,7 @@ def expand_map(rows: list[str]) -> list[str]:
     return new_rows
 
 
-def part_one(raw: str) -> int:
+def part_one_first_attempt(raw: str) -> int:
     rows = expand_map(utils.Input(raw).lines().strings)
     grid = utils.Grid(rows)
     galaxies = (point for point in grid.points if grid[point] == "#")
@@ -38,18 +38,10 @@ def part_one(raw: str) -> int:
     paths = []
     for pair in combinations:
         paths.append((utils.manhattan(*pair), pair))
-    breakpoint(context=10)
     return sum(path[0] for path in paths)
 
 
-def manhattan(
-    p1: utils.Point,
-    p2: utils.Point,
-) -> int:
-    return sum(abs(a - b) for a, b in zip(p1, p2, strict=True))  # type: ignore
-
-
-def part_two(raw: str, expand: int) -> int:
+def both_parts(raw: str, expand: int) -> int:
     rows = utils.Input(raw).lines().strings
     non_empty_cols = set()
     non_empty_rows = set()
@@ -63,27 +55,14 @@ def part_two(raw: str, expand: int) -> int:
     combinations = itertools.combinations(galaxies, 2)
     num_steps = 0
     for pair in combinations:
-        steps = 0
         p1, p2 = pair
         start_rn = min(p1[0], p2[0])
         start_cn = min(p1[1], p2[1])
         row_count, col_count = [abs(a - b) for a, b in zip(p1, p2, strict=True)]
-        # print(f"{p1=} {p2=} {start_rn=} {start_cn=} {row_count=} {col_count=}")
         for rn in range(start_rn + 1, start_rn + row_count + 1):
-            if rn not in non_empty_rows:
-                # print(f"{p1[0]=} {p2[0]=} {rn=} {expand=}")
-                steps += expand
-            else:
-                # print(f"{p1[0]=} {p2[0]=} {rn=} 1")
-                steps += 1
+            num_steps += 1 if rn in non_empty_rows else expand
         for cn in range(start_cn + 1, start_cn + col_count + 1):
-            if cn not in non_empty_cols:
-                # print(f"{p1[1]=} {p2[1]=} {cn=} {expand=}")
-                steps += expand
-            else:
-                # print(f"{p1[1]=} {p2[1]=} {cn=} 1")
-                steps += 1
-        num_steps += steps
+            num_steps += 1 if cn in non_empty_cols else expand
     return num_steps
 
 
@@ -98,14 +77,14 @@ def test():
 ..........
 .......#..
 #...#....."""
-    # answer_1 = part_one(test_input)
-    answer_2 = part_two(test_input, expand=10)
-    # assert answer_1 == 374, answer_1
+    answer_1 = both_parts(test_input, expand=2)
+    answer_2 = both_parts(test_input, expand=10)
+    assert answer_1 == 374, answer_1
     assert answer_2 == 1030, answer_2
 
 
 if __name__ == "__main__":
     test()
     data = aocd.get_data(day=11, year=2023)
-    print("Part 1: ", part_one(data))
-    print("Part 2: ", part_two(data, expand=1000000))
+    print("Part 1: ", both_parts(data, expand=2))
+    print("Part 2: ", both_parts(data, expand=1000000))
