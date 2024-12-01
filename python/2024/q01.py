07575ead0c3951ebf6a18e510c48657d57bf75a1
Josh Smeaton <josh.smeaton@gmail.com>
2024-12-01 17:01:46 +1100

    AOC Returns! 2024 Day 1

--- python/2024/q01.py
+++ python/2024/q01.py
@@ -3,17 +3,33 @@ import utils
 
 class Puzzle(utils.Puzzle):
     def part_one(self, input: utils.Input) -> str:
-        return ""
+        """
+        Find the distance between sorted pairs.
+        """
+        left, right = map(sorted, zip(*input.lines().split().numbers))
+        distance = sum(abs(a - b) for a, b in zip(left, right))
+        return str(distance)
 
     def part_two(self, input: utils.Input) -> str:
-        return ""
+        """
+        Find the similarity between sorted first column, summing left by the
+        count of matches in right.
+        """
+        left, right = map(sorted, zip(*input.lines().split().numbers))
+        similarity = sum(ln * right.count(ln) for ln in left)
+        return str(similarity)
 
 
 if __name__ == "__main__":
     runner = Puzzle(
         year=2024,
         day=1,
-        test_answers=("", ""),
-        test_input="""""",
+        test_answers=("11", "31"),
+        test_input="""3   4
+4   3
+2   5
+1   3
+3   9
+3   3""",
     )
     runner.cli()
