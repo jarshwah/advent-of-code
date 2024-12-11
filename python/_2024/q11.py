b1440916e81c46c5d2f0b33e850a4a52feccdaaf
Josh Smeaton <josh.smeaton@gmail.com>
2024-12-11 17:53:27 +1100

    Cleanup Day 11

--- python/_2024/q11.py
+++ python/_2024/q11.py
@@ -5,15 +5,28 @@ import utils
 
 @cache
 def mutate(stone: int, n: int) -> int:
+    """
+    Evolve a single stone N times.
+
+    Cache the results to avoid recalculating the same stone multiple times.
+
+    Stones evolve independently of one another, so results from one can feed the other.
+
+    If we're at the final step, return 1, otherwise, follow the rules:
+        1. If the stone is 0, return 1.
+        2. If the stone has an even number of digits, split it in half, and evolve each half.
+        3. Otherwise, multiply the stone by 2024.
+    """
     if n == 0:
         return 1
     if stone == 0:
         return mutate(1, n - 1)
     if len(str(stone)) % 2 == 0:
-        return mutate(int(str(stone)[: len(str(stone)) // 2]), n - 1) + mutate(
-            int(str(stone)[len(str(stone)) // 2 :]), n - 1
+        left, right = (
+            int(str(stone)[: len(str(stone)) // 2]),
+            int(str(stone)[len(str(stone)) // 2 :]),
         )
-
+        return mutate(left, n - 1) + mutate((right), n - 1)
     return mutate(stone * 2024, n - 1)
 
 
@@ -24,12 +37,19 @@ class Puzzle(utils.Puzzle):
         - even num digits -> [left-half, right-half]
         - otherwise -> x*2024
 
-    Then sum the number of stones after N iterations.
+    Then count the number of stones after N iterations.
     """
 
     def part_one(self, input: utils.Input) -> str | int:
         """
-        Loop 25 times
+        Loop 25 times.
+
+        The problem statement initially sounded like it'd be conways game of life, given
+        the statement that each stone changes simultaneously, which is why maintaining
+        a list of the actual values seemed important.
+
+        Part 2 didn't expand on that direction though. The simultaneous bit is useless
+        information, since each stone evolves independently in path parts.
         """
         stones = input.split().numbers
         for _ in range(25):
