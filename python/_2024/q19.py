aa22d3cd8f6227c87a781087d198a453eb24593a
Josh Smeaton <josh.smeaton@gmail.com>
2024-12-19 17:05:52 +1100

    2024 - Day 19

---
+++ python/_2024/q19.py
@@ -0,0 +1,47 @@
+from functools import cache
+
+import utils
+
+
+class Puzzle(utils.Puzzle):
+    def both_parts(self, input: utils.Input) -> tuple[str | int, str | int]:
+        [all_available], designs = input.group(sep="\n").strings
+        available = all_available.split(", ")
+
+        @cache
+        def check_pattern(design: str) -> int:
+            if not design:
+                return 1
+            matches = 0
+            for pattern in available:
+                if design.startswith(pattern):
+                    matches += check_pattern(design[len(pattern) :])
+            return matches
+
+        matches = 0
+        match_count = 0
+        for design in designs:
+            if cnt := check_pattern(design):
+                matches += 1
+                match_count += cnt
+        return matches, match_count
+
+
+if __name__ == "__main__":
+    runner = Puzzle(
+        year=2024,
+        day=19,
+        both=True,
+        test_answers=("6", "16"),
+        test_input="""r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb""",
+    )
+    runner.cli()
