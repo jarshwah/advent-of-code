7acdd32477850164de7e2fd7fe1ec09501708c45
Josh Smeaton <josh.smeaton@gmail.com>
2025-01-12 22:35:58 +1100

    2019 - Day 9

--- python/_2019/q09.py
+++ python/_2019/q09.py
@@ -11,14 +11,17 @@ class Puzzle(utils.Puzzle):
         return int(interpreter.run()[-1])
 
     def part_two(self, input: utils.Input) -> str | int:
-        return ""
+        program = input.split(",").numbers
+        interpreter = intcode.IntCode(program)
+        interpreter.input.write(2)
+        return int(interpreter.run()[-1])
 
 
 if __name__ == "__main__":
     runner = Puzzle(
         year=2019,
         day=9,
-        test_answers=("99", ""),
+        test_answers=("99", "99"),
         test_input="""109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99""",
     )
     runner.cli()
