f0a4a97a229fb9da7a573e44f82941528e6a0ee2
Josh Smeaton <josh.smeaton@gmail.com>
2025-12-01 22:06:31 +1100

    Adding a structure for csharp

---
+++ csharp/AoC/2025/Day01/Solution.cs
@@ -0,0 +1,21 @@
+namespace AdventOfCode.Y2025.Day01;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Numerics;
+
+[ProblemName("Secret Entrance")]
+class Solution : Solver {
+
+    public object PartOne(string input) {
+        return 0;
+    }
+
+    public object PartTwo(string input) {
+        return 0;
+    }
+}
---
+++ csharp/AoC/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+using AdventOfCode;
+
+var tsolvers = Assembly.GetEntryAssembly()!.GetTypes()
+    .Where(t => t.GetTypeInfo().IsClass && typeof(Solver).IsAssignableFrom(t))
+    .OrderBy(t => t.FullName)
+    .ToArray();
+
+var action =
+    Command(args, Args("update", "([0-9]+)/([0-9]+)"), m => {
+        var year = int.Parse(m[1]);
+        var day = int.Parse(m[2]);
+        return () => new Updater().Update(year, day).Wait();
+    }) ??
+    Command(args, Args("update", "today"), m => {
+        var dt = DateTime.UtcNow.AddHours(-5);
+        if (dt is { Month: 12, Day: >= 1 and <= 25 }) {
+            return () => new Updater().Update(dt.Year, dt.Day).Wait();
+        } else {
+            throw new AocCommuncationError("Event is not active. This option works in Dec 1-25 only)");
+        }
+    }) ??
+    Command(args, Args("upload", "([0-9]+)/([0-9]+)"), m => {
+        var year = int.Parse(m[1]);
+        var day = int.Parse(m[2]);
+        return () => {
+            var tsolver = tsolvers.First(tsolver =>
+                SolverExtensions.Year(tsolver) == year &&
+                SolverExtensions.Day(tsolver) == day);
+
+            new Updater().Upload(GetSolvers(tsolver)[0]).Wait();
+        };
+    }) ??
+    Command(args, Args("upload", "today"), m => {
+        var dt = DateTime.UtcNow.AddHours(-5);
+        if (dt is { Month: 12, Day: >= 1 and <= 25 }) {
+
+            var tsolver = tsolvers.First(tsolver =>
+                SolverExtensions.Year(tsolver) == dt.Year &&
+                SolverExtensions.Day(tsolver) == dt.Day);
+
+            return () =>
+                new Updater().Upload(GetSolvers(tsolver)[0]).Wait();
+
+        } else {
+            throw new AocCommuncationError("Event is not active. This option works in Dec 1-25 only)");
+        }
+    }) ??
+    Command(args, Args("([0-9]+)/(Day)?([0-9]+)"), m => {
+        var year = int.Parse(m[0]);
+        var day = int.Parse(m[2]);
+        var tsolversSelected = tsolvers.First(tsolver =>
+            SolverExtensions.Year(tsolver) == year &&
+            SolverExtensions.Day(tsolver) == day);
+        return () => Runner.RunAll(true, true, true, GetSolvers(tsolversSelected));
+    }) ??
+        Command(args, Args("[0-9]+"), m => {
+            var year = int.Parse(m[0]);
+            var tsolversSelected = tsolvers.Where(tsolver =>
+                SolverExtensions.Year(tsolver) == year);
+            return () => Runner.RunAll(true, true, true, GetSolvers(tsolversSelected.ToArray()));
+        }) ??
+    Command(args, Args("([0-9]+)/all"), m => {
+        var year = int.Parse(m[0]);
+        var tsolversSelected = tsolvers.Where(tsolver =>
+            SolverExtensions.Year(tsolver) == year);
+        return () => Runner.RunAll(true, true, true, GetSolvers(tsolversSelected.ToArray()));
+    }) ??
+    Command(args, Args("all"), m => {
+        return () => Runner.RunAll(true, true, true, GetSolvers(tsolvers));
+    }) ??
+    Command(args, Args("today"), m => {
+        var dt = DateTime.UtcNow.AddHours(-5);
+        if (dt is { Month: 12, Day: >= 1 and <= 25 }) {
+
+            var tsolversSelected = tsolvers.First(tsolver =>
+                SolverExtensions.Year(tsolver) == dt.Year &&
+                SolverExtensions.Day(tsolver) == dt.Day);
+
+            return () =>
+                Runner.RunAll(true, false, true, GetSolvers(tsolversSelected));
+
+        } else {
+            throw new AocCommuncationError("Event is not active. This option works in Dec 1-25 only)");
+        }
+    }) ??
+    Command(args, Args("calendars"), _ => {
+         return () => Runner.RunAll(false, true, false, GetSolvers(tsolvers));
+    }) ??
+    Command(args, Args("loc"), _ => {
+        return () => Runner.RunAll(false, false, true, GetSolvers(tsolvers));
+    }) ??
+    new Action(() => {
+        Console.WriteLine(Usage.Get());
+    });
+
+try {
+    action();
+} catch (AggregateException a) {
+    if (a.InnerExceptions.Count == 1 && a.InnerException is AocCommuncationError) {
+        Console.WriteLine(a.InnerException.Message);
+    } else {
+        throw;
+    }
+}
+
+Solver[] GetSolvers(params Type[] tsolver) {
+    return tsolver.Select(t => Activator.CreateInstance(t) as Solver).ToArray();
+}
+
+Action Command(string[] args, string[] regexes, Func<string[], Action> parse) {
+    if (args.Length != regexes.Length) {
+        return null;
+    }
+    var matches = Enumerable.Zip(args, regexes, (arg, regex) => new Regex("^" + regex + "$").Match(arg));
+    if (!matches.All(match => match.Success)) {
+        return null;
+    }
+    try {
+
+        return parse(matches.SelectMany(m =>
+                m.Groups.Count > 1 ? m.Groups.Cast<Group>().Skip(1).Select(g => g.Value)
+                                   : new[] { m.Value }
+            ).ToArray());
+    } catch {
+        return null;
+    }
+}
+
+string[] Args(params string[] regex) {
+    return regex;
+}
+
+class Usage {
+    public static string Get() {
+        return $"""
+            Usage: dotnet run [arguments]
+            1) To run the solutions and admire your advent calendar:
+
+             [year]/[day|all]      Solve the specified problems
+             today                 Shortcut to the above
+             [year]                Solve the whole year
+             all                   Solve everything
+
+             calendars             Show the calendars
+             loc                   Show the line of code charts
+
+            2) To start working on new problems:
+            login to https://adventofcode.com, then copy your session cookie, and export
+            it in your console like this
+
+             export SESSION=73a37e9a72a...
+
+            then run the app with
+
+             update [year]/[day]   Prepares a folder for the given day, updates the input,
+                                   the readme and creates a solution template.
+             update today          Shortcut to the above.
+
+            3) To upload your answer:
+            set up your SESSION variable as above.
+
+             upload [year]/[day]   Upload the answer for the selected year and day.
+             upload today          Shortcut to the above.
+
+            4) Don't forget to tip the maintainer https://github.com/sponsors/encse.
+            """;
+    }
+}
