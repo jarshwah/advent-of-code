ab2184bfa1f0a684ab7a0d10843197f48009db98
Josh Smeaton <josh.smeaton@gmail.com>
2025-12-02 00:32:03 +1100

    csharp 2025/01

--- csharp/AoC/2025/Day01/Solution.cs
+++ csharp/AoC/2025/Day01/Solution.cs
@@ -1,21 +1,44 @@
 namespace AdventOfCode.Y2025.Day01;
 
-using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
-using System.Numerics;
+
 
 [ProblemName("Secret Entrance")]
-class Solution : Solver {
+class Solution : Solver
+{
 
-    public object PartOne(string input) {
-        return 0;
+    static IEnumerable<(char direction, int clicks)> Parse(string input)
+    {
+        return input.Split("\n")
+                   .Select(line => (direction: line[0], clicks: int.Parse(line[1..])));
     }
 
-    public object PartTwo(string input) {
-        return 0;
+    public object PartOne(string input)
+    {
+        return Parse(input)
+            .Aggregate(
+                new { Position = 50, Zeros = 0 },
+                (acc, next) =>
+                {
+                    var movement = next.direction == 'R' ? next.clicks : -next.clicks;
+                    var newPosition = (acc.Position + movement) % 100;
+                    return new { Position = newPosition, Zeros = acc.Zeros + (newPosition == 0 ? 1 : 0) };
+                })
+            .Zeros;
+    }
+
+    public object PartTwo(string input)
+    {
+        return Parse(input)
+            .SelectMany(move => Enumerable.Range(0, move.clicks).Select(_ => move.direction == 'R' ? 1 : -1))
+            .Aggregate(
+                new { Position = 50, Zeros = 0 },
+                (acc, step) =>
+                {
+                    var newPosition = (acc.Position + step) % 100;
+                    return new { Position = newPosition, Zeros = acc.Zeros + (newPosition == 0 ? 1 : 0) };
+                })
+            .Zeros;
     }
 }
