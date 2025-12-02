2723d4a0643ca90c47db81c99b3078ebca6f5093
Josh Smeaton <josh.smeaton@gmail.com>
2025-12-02 20:03:45 +1100

    csharp 2025/01 - cleanup

--- csharp/AoC/2025/Day02/Solution.cs
+++ csharp/AoC/2025/Day02/Solution.cs
@@ -14,32 +14,42 @@ class Solution : Solver
 
     public object PartOne(string input)
     {
+        // If a repeating prefix exists, sum the matching product ids together.
+        // Must repeat exactly twice eg: 123123.
         return input.Split(",")
             .Select(range => range.Split("-"))
             .Select(pair => (long.Parse(pair[0]), long.Parse(pair[1])))
             .SelectMany(pair => Ranges.Range(pair.Item1, pair.Item2 + 1))
-            .Sum(productId => Repeating(productId, minimumLength: (int)Math.Ceiling((double)productId.ToString().Length / 2)));
+            .Sum(productId => IsRepeating(
+                productId,
+                minimumLength: (int)Math.Ceiling((double)productId.ToString().Length / 2))
+                ? productId : 0
+            );
     }
 
     public object PartTwo(string input)
     {
+        // If a repeating prefix exists, sum the matching product ids together.
+        // Must repeat at least twice eg: 123123123.
         return input.Split(",")
             .Select(range => range.Split("-"))
             .Select(pair => (long.Parse(pair[0]), long.Parse(pair[1])))
             .SelectMany(pair => Ranges.Range(pair.Item1, pair.Item2 + 1))
-            .Sum(productId => Repeating(productId, minimumLength: 1));
+            .Sum(productId => IsRepeating(productId, minimumLength: 1) ? productId : 0);
     }
 
 
 
-    static long Repeating(long productId, int minimumLength)
+    static bool IsRepeating(long productId, int minimumLength)
     {
         var strpid = productId.ToString();
         var strlen = strpid.Length;
+        // Generate all prefixes (up to half way) that match a minimum length
+        // Then concat them together N times, and check if it matches the original string.
         return Ranges.Range(1, strlen / 2 + 1)
             .Where(len => len >= minimumLength)
             .Select(len => strpid.Substring(0, len))
             .Where(prefix => string.Join("", Enumerable.Repeat(prefix, strlen / prefix.Length)) == strpid)
-            .Any() ? productId : 0;
+            .Any();
     }
 }
