fd9236e41be3a729ac66603bef989f88471edd01
Josh Smeaton <josh.smeaton@gmail.com>
2025-12-05 12:07:31 +1100

    python 2025/04

---
+++ python/_2025/q04.py
@@ -0,0 +1,57 @@
+from collections.abc import Sequence
+
+import utils
+
+
+class Puzzle(utils.Puzzle):
+    def part_one(self, input: utils.Input) -> str | int:
+        grid = input.grid()
+        found = list(grid.search(point_accessible, diagonal=True))
+        return len(found)
+
+    def part_two(self, input: utils.Input) -> str | int:
+        grid = input.grid()
+        removed = 0
+        loop = 0
+        animate = False
+        while True:
+            loop += 1
+            with grid.animate(animate) as animator:
+                animator.set_header(f"Loop: {loop} | Removed: {removed}")
+                found = list(grid.search(point_accessible, diagonal=True))
+                if not found:
+                    break
+                for f in found:
+                    grid[f[0]] = "X"
+                    removed += 1
+                    if animate:
+                        grid[f[0]] = utils.Color.YELLOW_BRIGHT.colorize("X")
+                        animator.update(grid)
+        return removed
+
+
+def point_accessible(
+    candidate: tuple[utils.Point, str], neighbours: Sequence[tuple[utils.Point, str]]
+) -> bool:
+    if candidate[1] != "@":
+        return False
+    return sum(1 for nb in neighbours if nb[1] == "@") < 4
+
+
+if __name__ == "__main__":
+    runner = Puzzle(
+        year=2025,
+        day=4,
+        test_answers=("13", "43"),
+        test_input="""..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.""",
+    )
+    runner.cli()
