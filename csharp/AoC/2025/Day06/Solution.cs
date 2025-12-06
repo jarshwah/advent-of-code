1e7f92abc30a56f5b6c06990eb91954edcff8ed2
Josh Smeaton <josh.smeaton@gmail.com>
2025-12-06 23:13:17 +1100

    csharp 2025/06

---
+++ csharp/AoC/2025/Day06/Solution.cs
@@ -0,0 +1,85 @@
+namespace AdventOfCode.Y2025.Day06;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Numerics;
+using static AdventOfCode.Utilities;
+using AngleSharp.Css.Dom;
+using AngleSharp.Text;
+using System.Runtime.Serialization.Formatters;
+
+[ProblemName("Trash Compactor")]
+class Solution : Solver
+{
+
+    public object PartOne(string input)
+    {
+        var tokenized = input.Split("\n").Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries));
+        return Transpose(
+            tokenized
+                .TakeWhile(tokens => int.TryParse(tokens.First(), out int intParse))
+                .Select(numList => numList.Select(int.Parse))
+        ).Zip(tokenized.Last())
+        .Select(zipped => zipped.Second == "*" ?
+            zipped.First.Aggregate(1L, (left, right) => left * right)
+            : zipped.First.Sum()
+        ).Sum();
+    }
+
+    public object PartTwo(string input)
+    {
+        var tokenized = Transpose(input.Split("\n"));
+        var nums = tokenized
+            .Where(line => !string.IsNullOrWhiteSpace(string.Concat(line)))
+            .Select(line =>
+                int.Parse(new string([.. line.Where(c => int.TryParse(c.ToString(), out int intParse))]))
+        ).GetEnumerator();
+        var operators = tokenized.SelectMany(line => line.Where(ch => "+*".Contains(ch))).GetEnumerator();
+        var equationBreaks = tokenized.Select(line => line.All(ch => ch == ' '));
+        return Calculate(nums, operators, equationBreaks).Sum();
+    }
+
+    private static IEnumerable<long> Calculate(
+        IEnumerator<int> nums,
+        IEnumerator<char> operators,
+        IEnumerable<bool> equationBreaks
+    )
+    {
+        nums.MoveNext();
+        operators.MoveNext();
+        List<int> currentNums = [];
+        foreach (var isBreak in equationBreaks)
+        {
+            if (isBreak)
+            {
+                yield return Aggregate(operators, currentNums);
+                operators.MoveNext();
+                currentNums.Clear();
+                continue;
+            }
+            currentNums.Add(nums.Current);
+            nums.MoveNext();
+        }
+        yield return Aggregate(operators, currentNums);
+
+        static long Aggregate(IEnumerator<char> operators, List<int> currentNums)
+        {
+            return operators.Current == '*'
+                ? currentNums.Aggregate(1L, (left, right) => left * right)
+                : currentNums.Sum();
+        }
+    }
+
+    private static IEnumerable<IEnumerable<T>> Transpose<T>(IEnumerable<IEnumerable<T>> rows)
+    {
+        var length = rows.First().Count();
+        for (int i = 0; i < length; i++)
+        {
+            yield return rows.Select(row => row.Skip(i).First());
+        }
+    }
+}
